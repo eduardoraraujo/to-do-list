37a9ec94438d147fc16ddd5ba2c0593f2ba1d680
Eduardo Ronchi de Araujo <eduardoaraujo2609@gmail.com>
2025-03-04 11:47:24 -0300

    finalizando a criação de variáveis globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import "./global.css";
+import styles from "./App.module.css";
 
 function App() {
-  return (
-    <>
-      <div>Olá</div>
-    </>
-  );
+  return <div className={styles.div}>Olá</div>;
 }
 
 export default App;
