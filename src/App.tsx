3dc90b4c6d5ab1a8691c87eff21471f5b07c32d1
Eduardo Ronchi de Araujo <eduardoaraujo2609@gmail.com>
2025-03-04 14:22:16 -0300

    finalizando estilização do input e button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header";
+import { InputTask } from "./components/InputTask";
 import "./global.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <InputTask />
+      </main>
     </div>
   );
 }
