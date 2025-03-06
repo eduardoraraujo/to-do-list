5b54550e52e1e23966a8a078f8c83ecd4730b9c9
Eduardo Ronchi de Araujo <eduardoaraujo2609@gmail.com>
2025-03-06 16:04:18 -0300

    instalações de libs e ajustes pontuais

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import { Header } from "./components/Header";
-import { InputTask } from "./components/InputTask";
+import { v4 as uuidv4 } from "uuid";
 import "./global.css";
+import { InputTask } from "./components/InputTask";
+import { Task } from "./components/Task";
+
+interface TaskProps {
+  id: string;
+  isCompleted: boolean;
+  title: string;
+}
+
+const tasks: TaskProps[] = [
+  {
+    id: uuidv4(),
+    isCompleted: true,
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+  },
+  {
+    id: uuidv4(),
+    isCompleted: false,
+    title:
+      "Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.",
+  },
+];
+
+const tasksCreated = tasks.length;
+const tasksCompleted = tasks.filter((task) => task.isCompleted).length;
 
 function App() {
   return (
     <div>
       <Header />
+      <InputTask />
       <main>
-        <InputTask />
+        <Task tasksCreated={tasksCreated} tasksCompleted={tasksCompleted} />
       </main>
     </div>
   );
